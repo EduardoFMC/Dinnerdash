90e5b1dcc26801c8b79be02e53acb72c992f1bce
Eduardo Ferreira <eduardo.ferreira.mc.edu@gmail.com>
2022-04-10 21:31:52 -0300

    projeto CJR

---
+++ coisa2.js
@@ -0,0 +1,9 @@
+let users = [
+    {id:1,
+    first_name:"Lauren",last_name:"Shaxby",email:"lshaxby0@php.net",created_at:"16/10/2021"},
+    {id:2,first_name:"Ardenia",last_name:"Paddingdon",email:"apaddingdon1@nsw.gov.au",
+    created_at:"27/07/2021"},
+    {id:3,first_name:"Renaldo",last_name:"Alenichev",email:"ralenichev2@ftc.gov"
+    ,created_at:"10/06/2021"}
+]
+
